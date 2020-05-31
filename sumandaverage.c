2ea714b7faebcb04f3757cf4ce26e5497c563882
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 19:34:43 +0530

    Update sumandaverage.c

--- sumandaverage.c
+++ sumandaverage.c
@@ -4,19 +4,19 @@
 #include<stdio.h>
 int main()
 {
-    int a[50],n,i;
+    int array[20],n,i;
     float sum=0;
     printf("Enter the number of elements of array\n");
     scanf("%d",&n);
     printf("Enter %d elements to the array\n",n);
     for(i=0;i<n;i++)
     {
-      scanf("%d",&a[i]);
-      sum=sum+a[i];
+      scanf("%d",&array[i]);
+      sum=sum+array[i];
     }
     printf("\nThe array elements are\n");
     for(i=0;i<n;i++)
-      printf("%d\t",a[i]);
-    printf("\nThe sum = %.2f , average = %.2f\n",sum,sum/n);
+      printf("%d\t",array[i]);
+      printf("\nThe sum = %.2f , average = %.2f\n",sum,sum/n);
     return 0;
 }
