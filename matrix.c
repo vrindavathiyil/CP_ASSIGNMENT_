076803019dbff07daf02adb48be31a7f87deb514
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 13:38:50 +0530

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,5 @@
+/*Write a menu driven program for performing matrix addition, multiplication and finding the
+transpose. Use functions to (i) read a matrix, (ii) find the sum of two matrices, (iii) find the product
+of two matrices, (i) find the transpose of a matrix and (v) display a matrix*/
+
+
