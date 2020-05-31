5733fcc2f584c0a6e8d145684d1518f2ba457e32
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 12:39:25 +0530

    Update euclideanspace.c

--- euclideanspace.c
+++ euclideanspace.c
@@ -9,9 +9,9 @@ struct Point
   }point1,point2,point3;
   int main()
     {
-      printf("Enter the first point\n"); 
+      printf("Enter the first point(dist1,dist2)\n"); 
       scanf("%d,%d",&point1.dist1,&point1.dist2);
-      printf("Enter the second point\n"); 
+      printf("Enter the second point(dist1,dist2)\n"); 
       scanf("%d,%d",&point2.dist1,&point2.dist2);
       point3.dist1=point1.dist1+point2.dist1;
       point3.dist2=point1.dist2+point2.dist2;
