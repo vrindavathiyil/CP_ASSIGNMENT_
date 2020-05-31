59349999827a4b6d6b30a4831e3eeeb5a8490185
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 12:03:26 +0530

    Update Concatenated string.c

--- Concatenated string.c
+++ Concatenated string.c
@@ -1,25 +1,27 @@
+/*Read two strings, store them in arrays and concatenate them without using library functions*/
+
 #include<stdio.h>
 void main()
     {
-       char S1[15],S2[20],S3[30];
+       char str1[15],str2[20],str3[30];
        int i=0,j=0;
        printf("Enter the first string ending with $");
-       scanf("%s",S1);
+       scanf("%s",str1);
        printf("Enter the second string ending with $");
-       scanf("%s",S2);
-       while(S1[i]!='\0')
+       scanf("%s",str2);
+       while(str1[i]!='\0')
            {
-              S3[j]=S1[i];
+              str3[j]=str1[i];
               i++;
               j++;
            }
        i=0;
-       while(S2[i]!='\0')
+       while(str2[i]!='\0')
            {
-             S3[j]=S2[i];
+             str3[j]=str2[i];
              i++;
              j++;
            }
-       S3[j]= '\0';
+       str3[j]= '\0';
        printf("concatenated string is:");
     }
