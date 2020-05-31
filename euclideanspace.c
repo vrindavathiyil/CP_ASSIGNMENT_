5ff1de3ca39d987c5a9221f6b54981d801d05e27
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 12:23:18 +0530

    Create euclideanspace.c

---
+++ euclideanspace.c
@@ -0,0 +1,20 @@
+/*Read two input each representing the distances between two points in the Euclidean space,
+store these in structure variables and add the two distance values*/
+
+#include <stdio.h>
+struct Point
+  {
+   int dist1,dist2;
+
+  }point1,point2,point3;
+  int main()
+    {
+      printf("Enter the first point\n"); 
+      scanf("%d,%d",&point1.dist1,&point1.dist2);
+      printf("Enter the second point\n"); 
+      scanf("%d,%d",&point2.dist1,&point2.dist2);
+      point3.dist1=point1.dist1+point2.dist1;
+      point3.dist2=point1.dist2+point2.dist2;
+      printf("new point after addition\n");
+      printf("(%d,%d)\n",point3.dist1,point3.dist2);
+    }
