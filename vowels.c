1271810402745c3c5da2d050b42ab6c646a26926
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 11:54:39 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,20 @@
+/* to count the number of vowels,consonants and spaces*/
+
+#include<stdio.h>
+void main()
+     {
+        char str[30];
+        int i,vowels=0,consonants=0,spaces=0;
+        printf("Enter the string with $\n");
+        gets(str);
+        for(i=0;str[i]!='/0';i++)
+            {
+               if(str[i]==' ')
+                  spaces++;
+               else if(str[i]=='a'|| str[i]=='e'|| str[i]=='i'|| str[i]=='o'|| str[i]=='u'||)
+                  vowels++;
+               else
+                  consonants++;
+        printf("vowels=%d consonants=%d spaces=%d,vowels,consonants,spaces);
+      }        
+       
