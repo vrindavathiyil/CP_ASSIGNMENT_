7aa9c21fae7cfcfcb8ef2871a27369931a0087c1
VRINDA VATHIYIL <62105596+vrindavathiyil@users.noreply.github.com>
2020-05-31 19:25:04 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+/*Read a Natural Number and check whether the number is Armstrong or not*/
+
+
+#include 
+<stdio.h>
+int main()
+{
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;  
+        result += remainder * remainder * remainder;
+        originalNum /= 10;
+
+}
+
+    if (result == num)
+        printf("%d is an Armstrong number", num);
+    else
+        printf("%d is not an Armstrong number", num);
+
+    return 0;
+}
